ed866964e0a6066056fe8bd0ddb6bc6792feb419
isabellaes <borjessonisabella@gmail.com>
2024-10-18 12:16:36 +0200

    create router

---
+++ client/src/pages/event-detail/EventDetailPage.jsx
@@ -0,0 +1,13 @@
+import Container from "@mui/material/Container";
+
+const EventDetailPage = () => {
+  return (
+    <Container
+      sx={{ backgroundColor: "white", minHeight: "100vh", minWidth: "90vw" }}
+    >
+      <p>hello world event detail</p>
+    </Container>
+  );
+};
+
+export default EventDetailPage;
