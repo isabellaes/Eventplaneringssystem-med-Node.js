0c25ed1ebd9a897c8bd129204fa0b8fc1232d043
Eva-Li80 <evaliboden@gmail.com>
2024-10-18 13:02:42 +0200

    header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,16 +4,20 @@ import HomePage from "./pages/home/HomePage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import EventDetailPage from "./pages/event-detail/EventDetailPage";
 import CreateEventPage from "./pages/create-event/CreateEventPage";
+import Header from "./components/header/Header";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<HomePage />} />
-        <Route path="/eventdetail/:Id" element={<EventDetailPage />} />
-        <Route path="/createevent" element={<CreateEventPage />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+    <Header/>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<HomePage />} />
+          <Route path="/eventdetail/:Id" element={<EventDetailPage />} />
+          <Route path="/createevent" element={<CreateEventPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import styles from "./header.module.scss"
+import Menu from "../menu/Menu";
+import MenuIcon from '@mui/icons-material/Menu';
+import PositionedMenu from '../menu/Menu';
+import Button from '@mui/material/Button';
+
+const Header = () => {
+    return (
+        <div className={styles.header}>
+            <MenuIcon className={styles.menu} />
+            <h1 className={styles.title}>Eventhanterare</h1>
+            <Button  variant="contained" color='success'>Skapa nytt event</Button>
+        </div>
+    )
+}
+
+export default Header
