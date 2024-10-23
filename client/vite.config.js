93d3aa9aaf9c221c3faa58f97ecf95d7b138905d
isabellaes <borjessonisabella@gmail.com>
2024-10-23 21:13:12 +0200

    add proxy config for vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
+      "/organizers": {
+        target: "http://localhost:3000/",
         changeOrigin: true,
-        secure: false
-      }
-    }
-  }
-})
+        secure: false,
+      },
+      "/events": {
+        target: "http://localhost:3000/",
+        changeOrigin: true,
+        secure: false,
+      },
+      "/users": {
+        target: "http://localhost:3000/",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
