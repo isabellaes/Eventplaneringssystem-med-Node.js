c33365d9288ec733fea1d99bec1bcdd335ef2576
naathaliie <nathalielarsson@live.se>
2024-10-23 15:06:33 +0200

    add userrouter in server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import { EventRouter } from "./routes/eventRouter.js";
 import Express from "express";
 import { OrganizerRouter } from "./routes/organizerRouter.js";
+import { UserRouter } from "./routes/userRouter.js";
 
 const app = Express();
 const port = 3000;
@@ -9,6 +10,7 @@ app.use(Express.json());
 
 app.use("/events", EventRouter());
 app.use("/organizers", OrganizerRouter());
+app.use("/users", UserRouter());
 
 app.listen(port, () => {
   console.log("server startat på port " + port);
