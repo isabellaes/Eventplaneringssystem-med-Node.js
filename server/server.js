5053a668fd066a31991d2b863b8366ca4ab4617c
Eva-Li80 <evaliboden@gmail.com>
2024-10-23 14:58:47 +0200

    organizerroter

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import { EventRouter } from "./routes/eventRouter.js";
 import Express from "express";
+import { OrganizerRouter } from "./routes/organizerRouter.js";
 
 const app = Express();
 const port = 3000;
@@ -7,6 +8,7 @@ const port = 3000;
 app.use(Express.json());
 
 app.use("/events", EventRouter());
+app.use("/organizers", OrganizerRouter());
 
 app.listen(port, () => {
   console.log("server startat på port " + port);
